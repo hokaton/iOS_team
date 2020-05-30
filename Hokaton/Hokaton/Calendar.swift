09f242db1da4c69570ba0a76bf1f770e01d84217
SeongEun <55864257+nkrang@users.noreply.github.com>
2020-05-31 04:35:59 +0900

    쪼끔 건드림

---
+++ Hokaton/Hokaton/Calendar.swift
@@ -0,0 +1,18 @@
+//
+//  Calendar.swift
+//  Hokaton
+//
+//  Created by SeongEun on 2020/05/31.
+//  Copyright © 2020 jaeyong Lee. All rights reserved.
+//
+
+import UIKit
+
+struct Calendar {
+    var month: String
+    
+    init(month: String) {
+        self.month = month
+    }
+    
+}
---
+++ Hokaton/Hokaton/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  Hokaton
+//
+//  Created by SeongEun on 2020/05/31.
+//  Copyright © 2020 jaeyong Lee. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    private var calendarList: [Calendar] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
